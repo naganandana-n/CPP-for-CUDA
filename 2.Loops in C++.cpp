b1fb8f9f841fd938629da015fc1b5099d94e4bc4
naganandana <naganandana17@gmail.com>
2025-01-30 10:44:43 +0400

    loops

---
+++ 2.Loops in C++.cpp
@@ -0,0 +1,12 @@
+// 2. Loops
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // very similar to c code
+    for(int i = 0; i < 5; i++){
+        cout << "Iteration " << i << "\n";
+    }
+    return 0;
+}
