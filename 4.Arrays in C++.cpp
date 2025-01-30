e2d942c243b4c80ec2590484567e51762c11031c
naganandana <naganandana17@gmail.com>
2025-01-30 11:14:07 +0400

    arrays

---
+++ 4.Arrays in C++.cpp
@@ -0,0 +1,10 @@
+// Arrays
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numbers[5] = {10, 2, 3, 4, 5}; // {} not []
+    cout << "First number: " << numbers[0] << endl;
+    return 0;
+}
